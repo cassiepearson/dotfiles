6db544803ed3557fc7d6bc9e15354597acc30d1e
Chris Negrich <cnegrich@gmail.com>
2020-01-14 13:01:39 -0700

    Responding to issue found at work. Fixed ZSH env var path.

--- .zsh/profile.zsh
+++ .zsh/profile.zsh
@@ -2,7 +2,7 @@
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH=/Users/chne/.oh-my-zsh
+export ZSH=$HOME/.oh-my-zsh
 
 # Source Cargo Paths
 source $HOME/.cargo/env
